7a8733472859defd21346349b826a3077d0c204f
Atilian <gascon.mickael@outlook.fr>
2021-12-05 07:28:54 +0100

    Refactorisation du dossier source avec ajout de 3 dossier -components -styles -assets ainsi que ajout du code css et js pour les composents banner et cart

---
+++ src/components/Banner.js
@@ -0,0 +1,16 @@
+import logo from "../assets/leaf.png"
+import "../styles/Banner.css"
+
+function Banner () {
+
+    const title = "La maison jungle";
+
+    return (
+        <div className="lmj-banner">
+            <img src={ logo } alt={ title } className="lmj-logo"/>
+            <h1 className="lmj-title">La maison jungle</h1>
+        </div>
+    )
+}
+
+export default Banner;
